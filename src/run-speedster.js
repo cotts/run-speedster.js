e64396e30d8405306c10f23cf7d05af1891866c3
Thadeu Cotts <thadeu.cotts@gmail.com>
2017-06-29 23:39:46 -0300

    increased script with options and some bug fix

--- src/run-speedster.js
+++ src/run-speedster.js
@@ -1,80 +1,188 @@
-var corpo = document.querySelector('.flash-run');
-var flash = document.createElement('span');
-let tam = corpo.offsetWidth;
+let runnerShadow = [];
+let size = 0;
+let blurSize = 0;
+let runSize = 0;
+let show = -100;
+let traveled = 0;
+let last = false;
+let textRunning;
+var runtext;
+function setDetails(config) {
+    details = config;
+
+    details.runner = config.runner || 'standard'
+    details.howBig = config.howBig || 50;
+    details.disappear = config.disappear || false;
+    details.speed = config.speed || 1;
 
 
-//Faz o elemento sumir ou não
-//1 = true, 2 = false
-let vanish = 2;
-
-
-flash.className = 'flash';
-
-Object.assign(flash.style, {
-    'background': 'url(https://cotts.tech/flash.svg) center no-repeat',
-    'background-size': '50px 50px',
-    'width': '50px',
-    'height': '50px',
-    'position': 'absolute'
-   
-
-});
-
-
-corpo.appendChild(flash);
-var flasharr = [];
-tam = tam * 0.80;
-var count;
-
-var path = 0;
-var i = 0;
-
-function createShadow(size) {
-    return new Promise((resolve, reject) => {
-        for (let i = 0; i <= size; i++) flasharr.push(flash.cloneNode(true));
-    });
-};
-function createMotion(shadow) {
-    return new Promise((resolve, reject) => {
-        shadow.forEach((element, index, ar) => {
-            
-            element.style.left = index * 1.4 + 'px';
-            element.style.opacity = (index * 0.0001);
-        });
-    })
-}
-
-function moveSpeedster(speedster, position) {
-    return new Promise((resolve, reject) =>{
-        speedster.style.left = position * 1.4 + 'px';
-    });
+    return Promise.resolve(details);
 };
 
-function addShadow(element) {
-    corpo.appendChild(element);
-}
+var createRunner = async (racePath,runText, config) => {
+    let path = document.querySelector(racePath);
+    runtext = document.querySelector(runText);
+    let sizeOfPath = path.offsetWidth;
+    let speedster = document.createElement('span');
+    textRunning = path.offsetWidth * -1;
+    setDetails(config)
+        .then((properties) => {
+            details = properties;
+            Object.assign(speedster.style, {
+                'background': `url(img/${details.runner}.svg) center no-repeat`,
+                'background-size': `${details.howBig}px ${details.howBig}px`,
+                'width': `${details.howBig}px`,
+                'height': `${details.howBig}px`,
+                'position': 'absolute',
+                'opacity': 0,
+                'left': `${path.offsetLeft}px`
+            });
+            Object.assign(speedster, {
+                config: {
+                    speed: details.speed,
+                    path: path,
+                    init: path.offsetLeft,
+                    sizeOfPath: sizeOfPath,
+                    disappear: details.disappear,
+                    howBig: details.howBig
+                },
 
-function removeShadow(element, position, vanish){
-    return new Promise((resolve, reject)=>{
-        setTimeout(() => element.removeChild(element.childNodes[vanish]), position * 0.8);
-    })
-}
-
-function runSpeedster(element) {
-    return new Promise((resolve,reject)=>{
-        element.forEach((element, index, arr) =>{
-            moveSpeedster(flash, index)
-                .then(addShadow(element))
-                .then(removeShadow(corpo, index, vanish));
+                className: 'speedster'
+            });
+            Object.assign(runtext.style, {
+                'left' : (path.offsetWidth - details.howBig) * -1 +'px',
+                'position' : 'absolute'
+            });
+           
+            path.appendChild(speedster);
         });
-    })
+
+    return await Promise.resolve(speedster);
+};
+
+
+
+let insertShadow = (el) => {
+    if (size === el.config.sizeOfPath - el.config.howBig) {
+        console.log('inseriu as sombras');
+
+        return;
+    }
+    runnerShadow.push({
+        span: el.cloneNode(true),
+        config: el.config
+    });
+    size++;
+    insertShadow(el);
+
+    return Promise.resolve(runnerShadow);
+}
+
+let createBlur = (shadow) => {
+    if (blurSize === shadow.length) {
+        console.log('fez o blur');
+        return;
+    }
+    Object.assign(shadow[blurSize].span.style, {
+        'left': `${shadow[blurSize].config.init + blurSize}px`,
+        'opacity': blurSize * 0.0001,
+        'z-index': blurSize * -1
+    });
+    blurSize++;
+    createBlur(shadow);
+    // await sleep(200);
+    return Promise.resolve(shadow);
+}
+
+let run = (el, loop) => {
+    let x = loop;
+    // speed of the object
+    var vel = el[0].config.speed;
+
+    //if the object will disappear in the end of the path
+    var disappear = el[0].config.disappear;
+
+    //max distance to run = iniial path + pathsize
+    var max =  el[0].config.sizeOfPath;
+
+    //initial position of the path
+    var init = el[0].config.init;
+
+    //the object that will be include the object and shadows
+    var path = el[0].config.path;
+
+    //actual position of the object
+    var posicao = path.firstElementChild.getBoundingClientRect().left;
+
+    //counter of the children objects inside the path way
+    var count = path.childElementCount;
+
+    //if get to the end, will make the next check or end
+    if (x >= max) {
+        console.log('correu');
+        return;
+    }
+
+    //if the way is in the last loop, change the velocity to not pass trough the path
+    if (posicao >= (el.length - vel - 2)) {
+        vel = max - posicao - el[0].config.howBig - 1;
+        textRunning = textRunning + 2;
+        last = true;
+    }
+    for (let i = 0; i <= vel; i++) {
+        if (typeof x === 'undefined') {
+            x = 0;
+        }
+        count = path.childElementCount;
+
+        if (posicao >= (max / 2) && count > vel && disappear) {
+
+            if (count === 1) {
+                path.removeChild(path.childNodes[1]);
+                console.log('correu e sumiu');
+                return;
+            } else {
+                path.removeChild(path.childNodes[1]);
+                path.removeChild(path.childNodes[1]);
+            }
+
+        }
+        if (posicao >= (max / 2) && count > vel && !disappear) {
+            if (count === 2) {
+                console.log('correu e ficou');
+                path.removeChild(path.childNodes[2]);
+                return;
+            } else {
+
+                path.removeChild(path.childNodes[2]);
+                path.removeChild(path.childNodes[2]);
+            }
+        }
+        path.firstElementChild.style.left = `${posicao + i}px`;
+        path.appendChild(runnerShadow[x + i].span);
+        runtext.style.left = `${textRunning + x + i +1}px`;
+    }
+    requestAnimationFrame(() => run(el, posicao - el[0].config.init + vel));
+    return Promise.resolve(el);
+};
+
+var showRunner = async (el) => {
+    if (show === 0) {
+        return;
+    };
+    el.style.opacity = 1 + (show / 100);
+    show++;
+    requestAnimationFrame(() => showRunner(el));
+    await sleep(2000);
+    return Promise.resolve(el);
+}
+
+async function sleep(long) {
+    function timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    await timeout(long);
 }
 
 
-setTimeout(() => {
-    createShadow(tam)
-        .then(createMotion(flasharr))
-        .then(runSpeedster(flasharr));
-}, 3000);
-
-            
+createRunner('.race-path','.run-text', { 'runner': 'standard', 'speed': 60, 'disappear': true }).then(showRunner).then(insertShadow).then(createBlur).then(run);
